2c2d442e670167f59f2c7e085807bdac3b26cf10
Mirko Akov <mirko.akov@icloud.com>
2023-12-01 23:15:33 +0200

    Setup GitHub actions (#1)

--- util/CopyToClipboard_test.go
+++ util/CopyToClipboard_test.go
@@ -1,9 +1,17 @@
 package util
 
-import "testing"
+import (
+	"runtime"
+	"testing"
+)
 
 // not a real test... used o run copy to clipboard without a main package
 func TestCopyToClipboard(t *testing.T) {
+	// Run this test only on mac
+	if runtime.GOOS != "darwin" {
+		t.Skip("Skipping test on non-macOS")
+	}
+
 	err := CopyToClipboard("advent of code")
 
 	if err != nil {
